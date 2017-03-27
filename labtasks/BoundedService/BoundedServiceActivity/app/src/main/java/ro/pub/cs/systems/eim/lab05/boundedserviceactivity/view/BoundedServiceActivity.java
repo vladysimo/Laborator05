75d197c32958b4f3d4e234aa57f114ef51e6ea3a
Andrei Vlad Postoaca <vlad.postoaca@gmail.com>
2017-03-27 18:21:16 +0300

    Solved tasks

--- labtasks/BoundedService/BoundedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/boundedserviceactivity/view/BoundedServiceActivity.java
+++ labtasks/BoundedService/BoundedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/boundedserviceactivity/view/BoundedServiceActivity.java
@@ -1,9 +1,16 @@
 package ro.pub.cs.systems.eim.lab05.boundedserviceactivity.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.sql.Timestamp;
 
 import ro.pub.cs.systems.eim.lab05.boundedserviceactivity.R;
 import ro.pub.cs.systems.eim.lab05.boundedserviceactivity.general.Constants;
@@ -18,6 +25,15 @@ public class BoundedServiceActivity extends AppCompatActivity {
     private int boundedServiceStatus = Constants.SERVICE_STATUS_UNBOUND;
 
     // TODO: exercise 10e - implement a button click listener for getMessageFromServiceButton
+    private class GetMessageButtonOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if (boundedService != null && boundedServiceStatus == Constants.SERVICE_STATUS_BOUND)
+                messageFromServiceTextView.setText("[" + new Timestamp(System.currentTimeMillis()) + "] "
+                        + boundedService.getMessage() + "\n"+ messageFromServiceTextView.getText());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +45,40 @@ public class BoundedServiceActivity extends AppCompatActivity {
 
         // TODO: exercise 10e - set an instance of the button click listener to handle click events
         // for getMessageFromServiceButton
+        getMessageFromServiceButton.setOnClickListener(new GetMessageButtonOnClickListener());
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         // TODO: exercise 10d - bind the service through an intent
+        Intent intent = new Intent(this, BoundedService.class);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
     protected  void onStop() {
         // TODO: exercise 10d - unbind the service
+        if (boundedServiceStatus == Constants.SERVICE_STATUS_BOUND) {
+            unbindService(serviceConnection);
+            boundedServiceStatus = Constants.SERVICE_STATUS_UNBOUND;
+        }
         super.onStop();
     }
 
     // TODO: exercise 10c - create a ServiceConnection object
     // override methods onServiceConnected() and onServiceDisconnected()
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            boundedService = ((BoundedService.BoundedServiceBinder)service).getService();
+            boundedServiceStatus = Constants.SERVICE_STATUS_BOUND;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            boundedService = null;
+            boundedServiceStatus = Constants.SERVICE_STATUS_UNBOUND;
+        }
+    };
 }
--- labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
+++ labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
@@ -5,11 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.TextView;
 
+import ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants;
+
 public class StartedServiceBroadcastReceiver extends BroadcastReceiver {
 
     private TextView messageTextView;
 
     // TODO: exercise 9 - default constructor
+    public StartedServiceBroadcastReceiver() {
+
+    }
 
     public StartedServiceBroadcastReceiver(TextView messageTextView) {
         this.messageTextView = messageTextView;
@@ -19,8 +24,27 @@ public class StartedServiceBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: exercise 7 - get the action and the extra information from the intent
         // and set the text on the messageTextView
+        String action = intent.getAction();
+        String information = null;
+        if (action.equals(Constants.ACTION_STRING)) {
+            information = intent.getStringExtra(Constants.DATA);
+        }
+        if (action.equals(Constants.ACTION_INTEGER)) {
+            information = String.valueOf(intent.getIntExtra(Constants.DATA, 0));
+        }
+        if (action.equals(Constants.ACTION_ARRAY_LIST)) {
+            information = intent.getStringArrayListExtra(Constants.DATA).toString();
+        }
 
         // TODO: exercise 9 - restart the activity through an intent
         // if the messageTextView is not available
+        if (messageTextView != null)
+            messageTextView.setText(messageTextView.getText() + "\n" + information);
+        else {
+            Intent startedServiceActivityIntent = new Intent(context, StartedServiceActivity.class);
+            startedServiceActivityIntent.putExtra(Constants.MESSAGE, information);
+            startedServiceActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            context.startActivity(startedServiceActivityIntent);
+        }
     }
 }
